87d1440c898636aa266610b5b6004ede6ba4b9b0
Armo777 <arman.sarkisyan.03@mail.ru>
2024-09-30 22:08:31 +0300

    Дописал класс Settings, который обрабатывает аргементы командной строки

--- src/main/java/ws/academy/Settings.java
+++ src/main/java/ws/academy/Settings.java
@@ -1,24 +1,35 @@
 package ws.academy;
 
-class Settings {
+public class Settings {
+    private String inputFilePath;
+    private String outputFilePath;
+    private boolean isCompress;
+    private boolean isExtract;
 
     Settings(String[] runArgs) {
-
+        for (int i = 0; i < runArgs.length; i++) {
+            switch (runArgs[i]) {
+                case "-c" -> isCompress = true;
+                case "-e" -> isExtract = true;
+                case "-input" -> inputFilePath = runArgs[++i];
+                case "-output" -> outputFilePath = runArgs[++i];
+            }
+        }
     }
 
     boolean isCompressOperation() {
-
+        return isCompress;
     }
 
     boolean isExtractOperation() {
-
+        return isExtract;
     }
 
     String getInputFilePath() {
-
+        return inputFilePath;
     }
 
     String getOutputFilePath() {
-
+        return outputFilePath;
     }
 }
